92910a0c3b1e6f77673cfcd71cc658727b0385c4
Guilherme Camargo <guilherme.ocamargo@gmail.com>
2019-02-05 23:40:15 -0200

    Denuncia and CategoriaDenuncia creation. Creation of repositories and builders

--- src/main/java/br/com/warnit/WarnItApplication.java
+++ src/main/java/br/com/warnit/WarnItApplication.java
@@ -1,9 +1,15 @@
 package br.com.warnit;
 
+import br.com.warnit.model.domain.CategoriaDenuncia;
+import br.com.warnit.model.domain.Denuncia;
 import br.com.warnit.model.domain.Logradouro;
 import br.com.warnit.model.domain.Usuario;
+import br.com.warnit.repository.CategoriaDenunciaRepository;
+import br.com.warnit.repository.DenunciaRepository;
 import br.com.warnit.repository.LogradouroRepository;
 import br.com.warnit.repository.UsuarioRepository;
+import br.com.warnit.util.builder.CategoriaDenunciaBuilder;
+import br.com.warnit.util.builder.DenunciaBuilder;
 import br.com.warnit.util.builder.LogradouroBuilder;
 import br.com.warnit.util.builder.UsuarioBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +24,10 @@ public class WarnItApplication implements CommandLineRunner {
 	private LogradouroRepository logradouroRepository;
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private DenunciaRepository denunciaRepository;
+	@Autowired
+	private CategoriaDenunciaRepository categoriaDenunciaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WarnItApplication.class, args);
@@ -33,6 +43,16 @@ public class WarnItApplication implements CommandLineRunner {
 		Usuario usuario = UsuarioBuilder.create().get();
 		usuario.setLogradouro(logradouro);
 		usuarioRepository.save(usuario);
+
+		/*Categoria Denuncia*/
+		CategoriaDenuncia categoriaDenuncia = CategoriaDenunciaBuilder.create().get();
+		categoriaDenunciaRepository.save(categoriaDenuncia);
+
+		/*Denuncia*/
+		Denuncia denuncia = DenunciaBuilder.create().get();
+		denuncia.setCategoria(categoriaDenuncia);
+		denuncia.setLocalidade(logradouro.getRua());
+		denunciaRepository.save(denuncia);
 	}
 }
 
---
+++ src/main/java/br/com/warnit/repository/CategoriaDenunciaRepository.java
@@ -0,0 +1,7 @@
+package br.com.warnit.repository;
+
+import br.com.warnit.model.domain.CategoriaDenuncia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaDenunciaRepository extends JpaRepository<CategoriaDenuncia, Long> {
+}
